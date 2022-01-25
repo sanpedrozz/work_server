44606060f6d4f730c86b5ee82b8ce8be8eb22f6d
sanpedro <sup686@gmail.com>
2022-01-25 11:39:53 +0300

    dell Task

--- queue_client/client.py
+++ queue_client/client.py
@@ -3,7 +3,6 @@ from functools import wraps
 from typing import List
 
 from redis.client import Redis
-from plc.task import Task
 from queue_client.redis_variables import SQL_INSERT
 
 
@@ -24,7 +23,7 @@ class RedisClient:
     def __init__(self):
         self.client = Redis()
 
-    def get_task(self, robot_name: str) -> Task:
+    def get_task(self, robot_name: str):
         task = self.client.lrange(robot_name, 0, 1)
         return pickle.loads(task[0]) if task else None
 
@@ -41,15 +40,6 @@ class RedisClient:
     def add_tasks(self, pid: str, tasks: List):
         tasks = [pickle.dumps(task) for task in tasks]
         return self.client.rpush(pid, *tasks)
-
-    def add_db_queue_task(self, task: Task):
-        self.client.rpush(SQL_INSERT, pickle.dumps(task))
-
-    def get_db_queue_task(self) -> Task:
-        task = self.client.lrange(SQL_INSERT, 0, 1)
-        if task:
-            task = pickle.loads(task[0])
-            return task
 
     def remove_db_queue_task(self) -> None:
         self.client.lpop(SQL_INSERT)
